5a3ddc302cee6124d0b3dff3ed5a53f17cd96bab
Eduardo Nascimento <eduardoplnascimento@gmail.com>
2022-05-16 18:37:14 -0300

    Add CRUD funcionalities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from database import db
 from controllers.HomeController import HomeController
+from controllers.ExpenseController import ExpenseController
 
 ###### CONFIGURACOES ######
 app = Flask('app')
@@ -16,6 +17,54 @@ def index():
 @app.route('/login')
 def login():
   return HomeController.login()
+
+@app.route('/register')
+def register():
+  return HomeController.register()
+
+@app.route('/signup', methods=['POST'])
+def signup():
+  return HomeController.signup()
+
+@app.route('/signin', methods=['POST'])
+def signin():
+  return HomeController.signin()
+
+@app.route('/logout')
+def logout():
+  return HomeController.logout()
+
+@app.route('/dashboard')
+def dashboard():
+  return ExpenseController.index()
+
+@app.route('/expenses/create')
+def expenses_create():
+  return ExpenseController.create()
+
+@app.route('/expenses/edit/<int:id>')
+def expenses_edit(id):
+  return ExpenseController.edit(id)
+
+@app.route('/expenses/store', methods=['POST'])
+def expenses_store():
+  return ExpenseController.store()
+
+@app.route('/expenses/update/<int:id>', methods=['POST'])
+def expenses_update(id):
+  return ExpenseController.update(id)
+
+@app.route('/expenses/pay/<int:id>')
+def expenses_pay(id):
+  return ExpenseController.pay(id)
+
+@app.route('/expenses/unpay/<int:id>')
+def expenses_unpay(id):
+  return ExpenseController.unpay(id)
+
+@app.route('/expenses/delete/<int:id>')
+def expenses_delete(id):
+  return ExpenseController.delete(id)
 
 ###### INICIALIZACAO ######
 with app.app_context():
